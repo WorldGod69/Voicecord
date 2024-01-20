5b6713338871539eefaa36c7c46fdf9345e7c725
WorldGod <80101825+WorldGod69@users.noreply.github.com>
2024-01-20 13:24:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from keep_alive import keep_alive
 
 status = "online" #online/dnd/idle
 
-GUILD_ID = ADD_YOUR_SERVER_ID_HERE
-CHANNEL_ID = ADD_YOUR_CHANNEL_ID_HERE
+GUILD_ID = 1094989461089165434
+CHANNEL_ID = 1134931963233439834
 SELF_MUTE = True
 SELF_DEAF = False
 
